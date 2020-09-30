b443cb9548efe79cf96ab67184909f57c2d9b280
David Hayes <drhayes@gmail.com>
2020-09-30 16:49:32 -0500

    Install tide. Function up.

--- .first-time-setup.fish
+++ .first-time-setup.fish
@@ -1,19 +1,36 @@
 #! /usr/bin/env fish
 
+function __drenv_message -a name --description "Show a message that things are present."
+  echo (set_color green)$name(set_color normal) is present.
+end
+
+# Grab the config.
 if not test -f ~/.config/fish/config.fish
   curl https://raw.githubusercontent.com/drhayes/drfish/main/config.fish -o ~/.config/fish/config.fish
 else
-  echo (set_color yellow)config.fish(set_color normal) is present.
+  __drenv_present config.fish
 end
 
+# Is tide there?
+if not type tide > /dev/null
+  curl -sL git.io/tide | source && tide_install
+else
+  __drenv_present tide
+end
+
+# Install fisher.
 if not type fisher > /dev/null
   curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 else
-  echo (set_color yellow)fisher(set_color normal) is present.
+  __drenv_present fisher
 end
 
+# Grab the fishfile.
 if not test -f ~/.config/fish/fishfile
   curl https://raw.githubusercontent.com/drhayes/drenv/main/fishfile -o ~/.config/fish/fishfile
 else
-  echo (set_color yellow)fishfile(set_color normal) is present.
+  __drenv_present fishfile
 end
+
+# Run fisher to grab our plugins.
+fisher
