de51c5632f936a8432c1a6f94fed4a0fcbe0d8b1
David Hayes <drhayes@gmail.com>
2020-10-01 11:16:21 -0500

    Fix function name. Add progress messages.

--- .first-time-setup.fish
+++ .first-time-setup.fish
@@ -1,36 +1,45 @@
 #! /usr/bin/env fish
 
-function __drenv_message -a name --description "Show a message that things are present."
+function _drenv_progress -a mesg --description "Show a progress message"
+  echo (set_color blue)$mesg
+end
+
+function _drenv_present -a name --description "Show a message that things are present."
   echo (set_color green)$name(set_color normal) is present.
 end
 
 # Grab the config.
+_drenv_progress Checking config...
 if not test -f ~/.config/fish/config.fish
   curl https://raw.githubusercontent.com/drhayes/drfish/main/config.fish -o ~/.config/fish/config.fish
 else
-  __drenv_present config.fish
+  _drenv_present config.fish
 end
 
 # Is tide there?
+_drenv_progress Checking tide...
 if not type tide -q
   curl -sL git.io/tide | source && tide_install --unattended
 else
-  __drenv_present tide
+  _drenv_present tide
 end
 
 # Install fisher.
+_drenv_progress Checking fisher...
 if not type fisher -q
   curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 else
-  __drenv_present fisher
+  _drenv_present fisher
 end
 
 # Grab the fishfile.
+_drenv_progress Checking fishfile...
 if not test -f ~/.config/fish/fishfile
   curl https://raw.githubusercontent.com/drhayes/drenv/main/fishfile -o ~/.config/fish/fishfile
 else
-  __drenv_present fishfile
+  _drenv_present fishfile
 end
 
 # Run fisher to grab our plugins.
+_drenv_progress Running fisher...
 fisher
